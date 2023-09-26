a7495f5cbeeea186f095853d9bf8e73087eeabd3
starkjoy <residualcode6@gmail.com>
2023-09-26 22:03:06 +0000

    Checked

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,14 +1,13 @@
 export default function createIteratorObject(report) {
-    // Create a generator function to yield each employee
-    function* employeeGenerator() {
-      for (const dep of Object.keys(report.allEmployees)) {
-        for (const empl of report.allEmployees[dep]) {
-          yield empl;
-        }
+  // Create a generator function to yield each employee
+  function* employeeGenerator() {
+    for (const dep of Object.keys(report.allEmployees)) {
+      for (const empl of report.allEmployees[dep]) {
+        yield empl;
       }
     }
-  
-    // Return an iterator
-    return employeeGenerator();
   }
-  
+
+  // Return an iterator
+  return employeeGenerator();
+}
