74f6699838c324e2fc921888b1b876809c4d881c
starkjoy <residualcode6@gmail.com>
2023-09-26 17:59:30 +0000

    Correct

--- 0x00-ES6_basic/11-createEmployeesObject.js
+++ 0x00-ES6_basic/11-createEmployeesObject.js
@@ -1,10 +1,9 @@
 export default function createEmployeesObject(departmentName, employees) {
-    // Create an object with the departmentName as the key and employees as the value
-    const employeesObject = {
-      [departmentName]: employees,
-    };
-  
-    // Return the created object
-    return employeesObject;
-  }
-  
+  // Create an object with the departmentName as the key and employees as the value
+  const employeesObject = {
+    [departmentName]: employees,
+  };
+
+  // Return the created object
+  return employeesObject;
+}
