cf15701e48ddbbd25f90252d3e8bb7909f31a17b
starkjoy <residualcode6@gmail.com>
2023-09-26 17:08:10 +0000

    Updated

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,7 +1,10 @@
 export default function appendToEachArrayValue(array, appendString) {
-  for (const value of array) {
-    array[array.indexOf(value)] = appendString + value;
+    /* eslint-disable no-loop-func */
+    for (const value of array) {
+      array[array.indexOf(value)] = appendString + value;
+    }
+    /* eslint-enable no-loop-func */
+  
+    return array;
   }
-
-  return array;
-}
+  
