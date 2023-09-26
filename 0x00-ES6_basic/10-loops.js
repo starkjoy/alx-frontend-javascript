b5cfccd7d9389407cb84a61b284650f9bba66207
starkjoy <residualcode6@gmail.com>
2023-09-26 15:16:05 +0000

    fixed

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,6 +1,8 @@
 export default function appendToEachArrayValue(array, appendString) {
-    for (let value of array) {
-      value = appendString + value;
+    for (const idx in array) {
+      if (array.hasOwnProperty(idx)) {
+        array[idx] = appendString + array[idx];
+      }
     }
   
     return array;
