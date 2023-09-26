cffc0b7b4692453402f56434781fb27a43e63095
starkjoy <residualcode6@gmail.com>
2023-09-26 21:21:43 +0000

    Commit

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,6 +1,10 @@
 export default function createReportObject(employeesList) {
     return {
-        allEmployees: {...employeesList,}, 
-        getNumberOfDepartments(empList) {return Object.keys(empList).length;},
-};
-  }
+        allEmployees: {
+            ...employeesList,
+        }, 
+        getNumberOfDepartments(empList) {
+            return Object.keys(empList).length;
+        },
+    };
+}
