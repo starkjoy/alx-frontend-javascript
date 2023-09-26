c274fca3195939cefd461f76dd3a4a2f57e433ee
starkjoy <residualcode6@gmail.com>
2023-09-26 17:45:46 +0000

    Newd

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,11 +1,10 @@
 export default function appendToEachArrayValue(array, appendString) {
-    // Loop through the array elements
+    let idx = 0;
     for (let value of array) {
-      // Append the string to each element and update the array
-      value = appendString + value;
+      array[idx] = appendString + value;
+      idx++;
     }
   
-    // Return the modified array
     return array;
   }
   
