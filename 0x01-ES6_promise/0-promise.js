c864cc5167390710dbdd34827f2c44cee19969b1
starkjoy <residualcode6@gmail.com>
2023-09-27 13:29:57 +0000

    Correct

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
 export default function getResponseFromAPI() {
     return new Promise((resolve, reject) => {
         const data = [resolve, reject];
-    })
-=======
-function getResponseFromAPI() {
-  return new Promise((resolve, reject) => {});
->>>>>>> c356f1ccfeb5e6d407abbff0db54a3e7a2f74f5e
+    });
 }
