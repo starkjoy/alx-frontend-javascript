c2319433d54421a91fb67cdcdb576faac5fab62d
starkjoy <residualcode6@gmail.com>
2023-09-27 22:59:31 +0000

    checked

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,13 +1,12 @@
 export default function guardrail(mathFunction) {
-    const queue = [];
-  
-    try {
-      const result = mathFunction();
-      queue.push(result, 'Guardrail was processed');
-    } catch (error) {
-      queue.push(error.message, 'Guardrail was processed');
-    }
+  const queue = [];
 
-    return queue;
+  try {
+    const result = mathFunction();
+    queue.push(result, 'Guardrail was processed');
+  } catch (error) {
+    queue.push(error.message, 'Guardrail was processed');
   }
-  
+
+  return queue;
+}
