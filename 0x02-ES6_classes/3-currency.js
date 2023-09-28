35df4ee91b9d110742e9bb0f50cdc4c7ff18c922
starkjoy <residualcode6@gmail.com>
2023-09-28 17:24:04 +0000

    Checked

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,35 @@
+export default class Currency {
+    constructor(code, name) {
+      this._code = code;
+      this._name = name;
+    }
+  
+    get getCode() {
+      return this._code;
+    }
+  
+    get getName() {
+      return this._name;
+    }
+  
+    set setCode(code) {
+      if (typeof code === 'string') {
+        this._code = code;
+      } else {
+        throw new TypeError("Wrong Code");
+      }
+    }
+  
+    set setName(name) {
+      if (typeof name === 'string') {
+        this._name = name;
+      } else {
+        throw new TypeError("Wrong Name");
+      }
+    }
+  
+    displayFullCurrency() {
+      return `${this._name} (${this._code})`; // Corrected the format here
+    }
+  }
+  
