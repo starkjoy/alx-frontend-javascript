70daaae264a8182f39679bb100124b904993ed6c
starkjoy <residualcode6@gmail.com>
2023-09-28 20:36:45 +0000

    Set

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,17 +1,16 @@
-export default class Building {
-  constructor(sqft) {
-    this._sqft = sqft;
+class Building {
+    constructor(sqft) {
+      this._sqft = sqft;
+    }
+  
+    // Getter for sqft
+    get sqft() {
+      return this._sqft;
+    }
+  
+    // Abstract method: evacuationWarningMessage
+    evacuationWarningMessage() {
+      throw new Error("Class extending Building must override evacuationWarningMessage");
+    }
   }
-
-  get getSqft() {
-    return this._sqft;
-  }
-
-  set setSqft(sqft) {
-    throw new Error("Property 'sqft' is read-only.");
-  }
-
-  evacuationWarningMessage() {
-    throw new Error('Class extending Building must override evacuationWarningMessage');
-  }
-}
+  
