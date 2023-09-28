ecbea1c39b38fe52fcce57df43d0f366e3a38450
starkjoy <residualcode6@gmail.com>
2023-09-28 20:40:53 +0000

    ES6 Class

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,16 +1,15 @@
 class Building {
-    constructor(sqft) {
-      this._sqft = sqft;
-    }
-  
-    // Getter for sqft
-    get sqft() {
-      return this._sqft;
-    }
-  
-    // Abstract method: evacuationWarningMessage
-    evacuationWarningMessage() {
-      throw new Error("Class extending Building must override evacuationWarningMessage");
-    }
+  constructor(sqft) {
+    this._sqft = sqft;
   }
-  
+
+  // Getter for sqft
+  get sqft() {
+    return this._sqft;
+  }
+
+  // Abstract method: evacuationWarningMessage
+  evacuationWarningMessage() {
+    throw new Error('Class extending Building must override evacuationWarningMessage');
+  }
+}
