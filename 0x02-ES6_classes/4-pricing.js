28ad2bb5623d24a13fb5ad73f392aaaea661e035
starkjoy <residualcode6@gmail.com>
2023-09-28 17:55:04 +0000

    fixed

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -31,7 +31,7 @@ export default class Pricing {
   }
 
   displayFullPrice() {
-    return `${this._amount} ${this._currency.getName()} (${this._currency.getCode()})`;
+    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
   }
 
   static convertPrice(amount, conversionRate) {
