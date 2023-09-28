902ee86bf3c2235067a460aa23ef0f0a2e51a463
starkjoy <residualcode6@gmail.com>
2023-09-28 21:02:16 +0000

    Test

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,27 +1,15 @@
 export default class Building {
-  constructor(sqft) {
-    if (this.constructor !== Building
+    constructor(sqft) {
+      if (this.constructor !== Building
           && typeof this.evacuationWarningMessage !== 'function') {
-      throw new Error('Class extending Building must override evacuationWarningMessage');
+        throw new Error('Class extending Building must override evacuationWarningMessage');
+      }
+  
+      this._sqft = sqft;
     }
-<<<<<<< HEAD
   
     get sqft() {
       return this._sqft;
     }
   }
   
-=======
-
-    this._sqft = sqft;
-  }
-
-  get sqft() {
-    return this._sqft;
-  }
-
-  evacuationWarningMessage() {
-    // Subclasses should provide their implementation
-  }
-}
->>>>>>> 2f64dd609d0628744eae724981374019fe39faa7
