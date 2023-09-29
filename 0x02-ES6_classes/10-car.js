d2b07bc0546d1e68748861b713e7927c37ad575d
starkjoy <residualcode6@gmail.com>
2023-09-29 16:35:29 +0000

    ES6 Classes

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,18 +1,18 @@
 export default class Car {
-	constructor(brand, motor, color) {
-		this._brand = brand;
-		this._motor = motor;
-		this._color = color;
-	}
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
 
-	cloneCar() {
-		const newObject = {};
-		const cloneSymbol = Symbol('clone'); // Create a new unique symbol
-		newObject[cloneSymbol] = {
-			_brand: this._brand,
-			_motor: this._motor,
-			_color: this._color
-		};
-		return newObject;
-	}
+  cloneCar() {
+    const newObject = {};
+    const cloneSymbol = Symbol('clone'); // Create a new unique symbol
+    newObject[cloneSymbol] = {
+      _brand: this._brand,
+      _motor: this._motor,
+      _color: this._color,
+    };
+    return newObject;
+  }
 }
