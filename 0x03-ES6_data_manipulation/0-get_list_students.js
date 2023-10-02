d0dbe4ecde5b8e72cecbfad2f5ac095670bc3fdb
starkjoy <residualcode6@gmail.com>
2023-10-02 21:28:08 +0000

    fix

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -2,6 +2,6 @@ export default function getListStudents() {
 	return [
 		{ firstName: 'Guillaume', id: 1, location: 'San Francisco' },
 		{ firstName: 'James', id: 2, location: 'Columbia' },
-		{ firstName: 'Serena', id: 3, location: 'San Francisco' }
+		{ firstName: 'Serena', id: 3, location: 'San Francisco' },
 	];
 }
