4e46a089d8a15cbe0566266511d06e816c02a0dd
starkjoy <residualcode6@gmail.com>
2023-10-02 23:37:40 +0000

    ES6 Data Manipulation

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,8 +1,7 @@
 export default function getStudentIdsSum(students) {
-    if (!Array.isArray(students)) {
-      return 0;
-    }
-  
-    return students.reduce((acc, cv) => acc + cv.id, 0);
+  if (!Array.isArray(students)) {
+    return 0;
   }
-  
+
+  return students.reduce((acc, cv) => acc + cv.id, 0);
+}
