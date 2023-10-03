948cc0298b4284a4613c15a13f16dc29e7aa5cc2
starkjoy <residualcode6@gmail.com>
2023-10-03 10:57:08 +0000

    ES6 Data Manipulation

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,19 +1,18 @@
 export default function cleanSet(set, startString) {
-    let result = '';
-    
-    for (const value of set) {
-      const stringValue = String(value);
-      
-      if (stringValue.startsWith(startString)) {
-        result += stringValue.substring(startString.length) + '-';
-      }
+  let result = '';
+
+  for (const value of set) {
+    const stringValue = String(value);
+
+    if (stringValue.startsWith(startString)) {
+      result += `${stringValue.substring(startString.length)}-`;
     }
-    
-    // Remove the trailing '-' if it exists
-    if (result.endsWith('-')) {
-      result = result.slice(0, -1);
-    }
-    
-    return result;
   }
-  
+
+  // Remove the trailing '-' if it exists
+  if (result.endsWith('-')) {
+    result = result.slice(0, -1);
+  }
+
+  return result;
+}
