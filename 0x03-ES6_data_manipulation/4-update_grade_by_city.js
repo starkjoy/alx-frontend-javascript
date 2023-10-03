50f44c625fab0b2b225677cdb029a98820e7fd21
starkjoy <residualcode6@gmail.com>
2023-10-03 00:16:32 +0000

    ver

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,17 +1,19 @@
 export default function updateStudentGradeByCity(students, city, newGrades) {
-  if (!Array.isArray(students) || typeof city !== 'string' || !Array.isArray(newGrades)) {
-    return [];
+    if (!Array.isArray(students) || typeof city !== 'string' || !Array.isArray(newGrades)) {
+      return [];
+    }
+  
+    return students
+      .filter((stu) => stu.location === city)
+      .map((student) => {
+        const gradeObj = newGrades.find((grade) => grade.studentId === student.id);
+        const updatedStudent = { ...student }; // Clone the student object
+        if (gradeObj) {
+          updatedStudent.grade = gradeObj.grade;
+        } else {
+          updatedStudent.grade = 'N/A';
+        }
+        return updatedStudent;
+      });
   }
-
-  return students
-    .filter((stu) => stu.location === city)
-    .map((student) => {
-      const gradeObj = newGrades.find((grade) => grade.studentId === student.id);
-      if (gradeObj) {
-        student.grade = gradeObj.grade;
-      } else {
-        student.grade = 'N/A';
-      }
-      return student;
-    });
-}
+  
