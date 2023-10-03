583484fc05e3c20a2ee9c2823c1917f933cc3462
starkjoy <residualcode6@gmail.com>
2023-10-03 10:54:32 +0000

    checked

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,19 @@
+export default function cleanSet(set, startString) {
+    let result = '';
+    
+    for (const value of set) {
+      const stringValue = String(value);
+      
+      if (stringValue.startsWith(startString)) {
+        result += stringValue.substring(startString.length) + '-';
+      }
+    }
+    
+    // Remove the trailing '-' if it exists
+    if (result.endsWith('-')) {
+      result = result.slice(0, -1);
+    }
+    
+    return result;
+  }
+  
