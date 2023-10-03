66a6dc7e6d5ee9badca32ff77d05d5f65d078730
starkjoy <residualcode6@gmail.com>
2023-10-03 12:05:49 +0000

    check

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,19 +1,8 @@
 export const weakMap = new WeakMap();
-
-export default function queryAPI(endpoint) {
+export function queryAPI(endpoint) {
   if (!weakMap.has(endpoint)) {
-    // Initialize a counter for this endpoint if it doesn't exist
     weakMap.set(endpoint, 0);
   }
-
-  // Get the current count for this endpoint
-  const count = weakMap.get(endpoint);
-
-  // Increment the count
-  weakMap.set(endpoint, count + 1);
-
-  // Check if the count is >= 5, and throw an error if so
-  if (count >= 5) {
-    throw new Error('Endpoint load is high');
-  }
+  weakMap.set(endpoint, weakMap.get(endpoint) + 1);
+  if (weakMap.get(endpoint) >= 5) throw Error('Endpoint load is high');
 }
